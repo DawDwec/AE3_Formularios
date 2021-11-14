c313eb01b6f8da24d216cf221e87442498d08d5f
DawDwec <91686488+DawDwec@users.noreply.github.com>
2021-11-14 18:50:00 +0100

    Delete js directory

--- js/precio.js
+++
@@ -1,6 +0,0 @@
-let texto
-if(confirm("El precio total de la pizza es de: " + resultado + " €")) {
-    texto = "Pedido realizado con exito!";
-} else {
-    texto = "Pedido cancelado con exito!";
-}
