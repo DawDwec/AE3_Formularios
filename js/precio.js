b69bac948a113a1c4be1dbbcbade05d23f8867d1
David Lopez <davidlofe7@gmail.com>
2021-11-14 19:08:37 +0100

    Se añade el PDF del ejercicio

--- js/precio.js
+++
@@ -1,7 +0,0 @@
-//Mensaje de confirmacion del pedido
-let texto
-if(confirm("El precio total de la pizza es de: " + resultado + " €")) {
-    texto = "Pedido realizado con exito!";
-} else {
-    texto = "Pedido cancelado con exito!";
-}
